19f51947b88b7b1232f2d6f56d9bc563647a6ae5
Juan Sánchez <judasane@gmail.com>
2022-05-05 10:43:45 -0500

    chore: Removed comment from jest.config

--- jest.config.ts
+++ jest.config.ts
@@ -1,8 +1,3 @@
-/*
- * For a detailed explanation regarding each configuration property and type check, visit:
- * https://jestjs.io/docs/configuration
- */
-
 export default {
   // Automatically clear mock calls, instances, contexts and results before every test
   clearMocks: true,
